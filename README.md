d307039ddbca39e9c6fab8a6ca64624cfc06b6f2
Polaris08 <tmdgus6237@naver.com>
2023-06-26 18:31:08 +0900

    Pybo Project First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+PROJECT : C:\projects\mysite
+
+PATH : C:\Users\Polaris\mysite.cmd
+
+python manage.py runserver
+
+http://127.0.0.1:8000/ (LOCAL)
---
+++ pybo/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
+
+class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
+
+
+"""
+models.py 변경 시 해야할 것:
+    python manage.py makemigrations
+    python manage.py migrate
+"""
